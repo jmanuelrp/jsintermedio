5fd783af35cc1966e115189a7a12592620dec9a6
JManuel Ruiz <metalfranckie@gmail.com>
2014-03-05 12:04:21 -0600

    Codigo inicial, uso de funciones como clases

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
+function Animal(nombre) {
+	this.nombre = nombre;
 
+	this.saludo = function(){
+
+		return this.nombre;
+	}
+}
+
+function Oso(nombre) {
+	this.nombre = nombre;
+}
+
+
+var bocho = new Oso('');
+
+Animal.prototype.patas = 0;
+
+Animal.prototype.getPatas = function(){
+	return this.patas;
+};
