b442bdfb0b127cadef3653766f3950da378b2873
José Manuel Ruiz <metalfranckie@gmail.com>
2014-03-05 13:31:25 -0500

    Mejora para codigo de ejemplo

--- js/main.js
+++ js/main.js
@@ -3,19 +3,23 @@ function Animal(nombre) {
 
 	this.saludo = function(){
 
-		return this.nombre;
-	}
+		return 'Hola! mi nombre es '+this.nombre;
+	};
 }
 
 function Oso(nombre) {
 	this.nombre = nombre;
 }
 
-
-var bocho = new Oso('');
+var pardo = new Oso('Pardo');
 
 Animal.prototype.patas = 0;
 
 Animal.prototype.getPatas = function(){
 	return this.patas;
 };
+
+console.log('Oso', pardo);
+console.log('Pertenece a "clase" Oso', pardo instanceof Oso);
+console.log('Nombre del oso', pardo.nombre);
+console.log('Numero de patas', pardo.getPatas());
