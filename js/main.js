fcd45ba7ed3abf28814669339b4642ce6f0a8df4
José Manuel Ruiz <metalfranckie@gmail.com>
2014-03-06 23:50:15 -0500

    Ejemplos de la segundo sesion del mini curso

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+console.log('Bienvenido al curso');
+
+var miObjeto = {
+    cadena: 'unacadena',
+    numero: 123
+};
+
+miObjeto.nuevoAtributo = 'nuevo';
+
+var obj;
+
 function Animal(nombre) {
 	this.nombre = nombre;
 
@@ -6,20 +17,34 @@ function Animal(nombre) {
 		return 'Hola! mi nombre es '+this.nombre;
 	};
 }
-
-function Oso(nombre) {
+//function Oso(nombre){
+var Oso = function(nombre, edad) {
 	this.nombre = nombre;
-}
+	
+	this.getEdad = function(){
+        return edad;
+	};
+};
 
-var pardo = new Oso('Pardo');
+Oso.prototype = new Animal();
+
+var pardo = new Oso('Pardo', 16);
+var nuevo = new Oso('Panda',9);
+nuevo.patas = 4;
 
 Animal.prototype.patas = 0;
 
-Animal.prototype.getPatas = function(){
+Animal.prototype.getPatas = function(uno, dos, tres){
 	return this.patas;
 };
 
+Oso.prototype.getEdad = function(){
+    return this.edad;
+};
+
 console.log('Oso', pardo);
-console.log('Pertenece a "clase" Oso', pardo instanceof Oso);
-console.log('Nombre del oso', pardo.nombre);
-console.log('Numero de patas', pardo.getPatas());
+console.log('Pertenece a "clase" Oso', nuevo instanceof Oso);
+console.log('Pertenece a "clase" Animal', nuevo instanceof Animal);
+console.log('Nombre del oso', nuevo.saludo.call(pardo));
+console.log('Numero de patas', nuevo.getPatas.call(pardo,1,2,3));
+console.log('Edad del oso', nuevo.getEdad.apply(pardo,[1,2,3]));
